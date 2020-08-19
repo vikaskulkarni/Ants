521c12f52de5242b8299da8cb479ac3532646058
vikaskulkarni <vikas.kulkarni@gmail.com>
2020-08-19 11:01:13 +0530

    server port

--- client/webpack/dev_server.js
+++ client/webpack/dev_server.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 const devServer = {
   quiet: false,
-  port: 65132, // Why 65132? R-15-1+5=6, E=5, A=1, C=3, T-20-2+0=2 => 65132
+  port: 65133, // Why 65132? R-15-1+5=6, E=5, A=1, C=3, T-20-2+0=2 => 65132
   proxy: { "/api/**": { target: 'http://localhost:3001', secure: false } },
   contentBase: join(__dirname, '..', 'dist'),
   hot: true,
